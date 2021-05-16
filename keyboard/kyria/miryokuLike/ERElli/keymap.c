b3204afbd10419851e2f967e7e3f99545f006767
Eric <EricRoy.Elli@gmail.com>
2021-05-15 22:31:01 -0230

    Update mouse buttons

--- keyboard/kyria/miryokuLike/ERElli/keymap.c
+++ keyboard/kyria/miryokuLike/ERElli/keymap.c
@@ -97,18 +97,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------.                                      ,-----------------------------------------------------.
  * |        |        |        |        |        |        |                                      |        |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------|                                      |--------+--------+--------+--------+--------+--------|
- * |        |  Ctrl  |  Alt   |  Cmd   | Shift  |        |                                      |        |  MC L  |  MC D  |  MC U  |  MC R  |        |
+ * |        |  Ctrl  |  Alt   |  Cmd   | Shift  |        |                                      |  MB1   |  MC L  |  MC D  |  MC U  |  MC R  |        |
  * |--------+--------+--------+--------+--------+--------+-----------------.  ,-----------------+--------+--------+--------+--------+--------+--------|
- * |        |        |        |        |        |        |        |        |  |        |        |        |  MW L  |  MW D  |  MW U  |  MW R  |        |
+ * |        |        |        |        |        |        |        |        |  |        |        |  MB2   |  MW L  |  MW D  |  MW U  |  MW R  |        |
  * `--------------------------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------------------------'
- *                            |        |        |        |        |        |  |  MB1   |  MB3   |  MB2   |        |        |
+ *                            |        |        |        |        |        |  |  MB1   |  MB2   |  MB3   |        |        |
  *                            `--------------------------------------------'  `--------------------------------------------'
  */
     [_MOUSE] = LAYOUT(
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT,   KC_NO,                                       KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,   KC_NO,
-                                   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_BTN1, KC_BTN3, KC_BTN2,   KC_NO,   KC_NO
+        KC_NO, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT,   KC_NO,                                     KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_BTN2, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,   KC_NO,
+                                   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_BTN1, KC_BTN2, KC_BTN3,   KC_NO,   KC_NO
     ),
 
 /*
